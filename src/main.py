98111ddcd6209eca5ac8d7577b593e9a451bad9c
Ethan Shry <ehshry@gmail.com>
2020-11-01 10:51:08 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,17 +1,21 @@
 from bottle import route, run, template
 @route('/')
 def index():
+    print("Standard")
     return "Hello, Index"
 
 @route('/fancy')
 def index():
+    print("FANCAY")
     return "Fancy Route~~~ :u"
 
 @route('/app/info')
 def app_info():
+    print("Seeking app info...")
     with open('./src/env.txt', 'r') as env:
         tmp = '<h1> Scapegoat App Info </h1>'
         for line in env.readlines():
+            print("line")
             tmp = f'{tmp} <h5>{line}</h5>'
         return tmp
 
